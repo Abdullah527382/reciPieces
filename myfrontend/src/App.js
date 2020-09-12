42c73f3061072edcc32e227f4e7a1af1e2e1333d
abdullah527382 <z5207998@unsw.edu.au>
2020-09-12 16:50:27 +1000

    submodule changes

---
+++ myfrontend/src/App.js
@@ -0,0 +1,15 @@
+import React from "react";
+import recipes from "./recipes.js";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <recipes />
+      </header>
+    </div>
+  );
+}
+
+export default App;
